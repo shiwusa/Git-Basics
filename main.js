272ec4f5cf17081f761e73a7fb8544e67fc097d7
shiwusa <lekh0t0er@gmail.com>
2022-02-20 19:47:10 +0200

    Added functions for reading numbers from console
    Reading data from console provided by prompt-sync module.
    inputFromConsole function reads arguments which are written by user and checks their validity.
    storeCoeffs function receives values of arguments and store them as an array.

---
+++ main.js
@@ -0,0 +1,27 @@
+'use strict'
+const prompt = require("prompt-sync")();
+
+const inputFromConsole = (arg) => {
+    while (true) {
+        const coeff = prompt(`Enter ${arg}: `);
+        const coeffValue = parseInt(coeff);
+        if (arg === "a" && coeffValue === 0) {
+            console.log(`Arg "a" cannot equal 0`);
+        } else if (isNaN(coeffValue)) {
+            console.log("Smth went wrong. Enter coefficient once more.");
+        } else return coeffValue;
+    }
+};
+
+const storeCoeffs = (...args) => {
+    const argArr = [...args] = ['a', 'b', 'c'];
+    const valuesArr = [];
+    argArr.forEach(coeff => {
+        const value = inputFromConsole(coeff);
+        valuesArr.push(value);
+    });
+    console.log(valuesArr);
+    // return paramsArr;
+};
+
+storeCoeffs();
