6abb55ab69bb57230dd916a5738170ab339ac23a
shiwusa <lekh0t0er@gmail.com>
2022-02-20 11:15:57 +0200

    created function to find the solutions of the quadratic equation
    Added function that gets 3 arguments and solve the quadratic equation using discriminant. This function also checks the value of discriminant (D) and prints results in console.

---
+++ findRoots.js
@@ -0,0 +1,18 @@
+
+const findRoots = (a, b, c) => {
+    let D = b * b - 4 * a * c;
+    if ( isNaN(D) ) {
+        console.log("Incorrect input");
+    } else if ( D < 0 ) {
+        console.log("There`s no roots");
+    } else if ( D === 0 ) {
+        let result = (-b + Math.sqrt(D)) / (2 * a);
+        console.log ("There`s 1 root: " + result);
+    } else if ( D > 0 ) {
+        let result1 = (-b + Math.sqrt(D)) / (2 * a);
+        let result2 = (-b - Math.sqrt(D)) / (2 * a);
+        console.log(`There are 2 roots:\nx1 = ${result1}\nx2 = ${result2}`);
+    }
+}
+
+module.exports = {findRoots}
