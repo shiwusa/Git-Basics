dcb03614e81461f8b440fe563e2977baaf01cc11
shiwusa <lekh0t0er@gmail.com>
2022-02-21 18:14:40 +0200

    Created new function to choose mode of the program
    Function "mode" allows user to choose in which mode program will work. For reading data from console also was used prompt-sync module.
    Also was changed the name of function from interactive mode for better understanding.

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 'use strict';
 
+const prompt = require("prompt-sync")();
 const findRoots = require("./findRoots");
-const storeCoeffs = require("./interactiveMode");
+const interactive = require("./interactiveMode");
+const nonInteractive = require("./non-interactiveMode");
 
-const argsArr = storeCoeffs();
-findRoots(argsArr);
+const mode = () => {
+    const progMode = parseInt(prompt("Choose the mode: "));
+    if (progMode === 1) {
+        const argsArr = interactive();
+        findRoots(argsArr);
+        mode();
+    } else if (progMode === 2) {
+        const argsArr = nonInteractive();
+        findRoots(argsArr);
+        mode();
+    } else {
+        console.log("Invalid mode. Try once more: ");
+        mode();
+    }
+}
+ mode();
